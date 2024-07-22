a94d7b463574ab51e7eba02ec5681956b990ba4a
Vishnu Doppalapudi <vishnu.doppalapudi@gmail.com>
2024-07-22 06:22:52 -0700

    comment

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 LINEAR_TEAM_ID = "a409ee5a-1f47-4e5f-bf16-332272fefacf"
 
+# Reading from text files from now to avoid pushing these keys...
 with open("openai_key.txt") as f:
     OPENAI_KEY = f.read().strip("\n")
 
